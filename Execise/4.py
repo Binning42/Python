51c7e513062f7012ecccfa98417260519584534e
Binning <hyy_41@live.com>
2018-10-10 19:55:33 +0800

    Python

--- Execise/4.py
+++ Execise/4.py
@@ -1,5 +1,13 @@
-h=int(input('Height='))
-w=int(input('Weight='))
-bmi=w/pow(h,2)
-if w>32
-print('too hea')
+h = float(input("Height="))
+w = float(input("Weight="))
+bmi = w / pow(h, 2)
+if bmi > 32:
+    print('terrible heavy')
+elif bmi < 18.5:
+    print('underweight')
+elif bmi <= 25:
+    print('normal')
+elif bmi <=28:
+    print('heavy')
+elif bmi <=32:
+    print('too heavy')
